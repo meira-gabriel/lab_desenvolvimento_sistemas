5a8d563dab4937becd520fe494bd28161170ed38
luizaamaria <luizamariabarros.contato@gmail.com>
2023-09-14 23:31:24 -0300

    Criado endpoints Restaurante

--- backend/app/core/urls.py
+++ backend/app/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UsuarioAPI
+from .views import UsuarioAPI, EnderecoAPI, EstabelecimentoAPI
 
 urlpatterns = [
     path('usuario/', UsuarioAPI.as_view()),
     path('usuario/<int:id>', UsuarioAPI.as_view()),
+    path('endereco/', EnderecoAPI.as_view()),
+    path('endereco/<int:id>', EnderecoAPI.as_view()),
 ]
--- backend/app/core/views.py
+++ backend/app/core/views.py
@@ -76,3 +76,104 @@ class UsuarioAPI(APIView):
 
         obj.delete()
         return Response({"msg":"deletado"}, status=status.HTTP_204_NO_CONTENT)
+
+class EnderecoAPI(APIView):
+    def get(self, request, id=None):
+        if id is None:
+            queryset = Endereco.objects.all()
+            serializer = EnderecoSerializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            try:
+                obj = Endereco.objects.get(pk=id)
+                serializer = EnderecoSerializer(obj)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except Endereco.DoesNotExist:
+                msg = {"msg": "Endereco não encontrado"}
+                return Response(status=status.HTTP_404_NOT_FOUND, data=msg)
+        serializer = EnderecoSerializer(obj)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = EnderecoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, id):
+        try:
+            obj = Endereco.objects.get(id=id)
+        except Endereco.DoesNotExist:
+            msg = {"msg": "Endereco não encontrado"}
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = EnderecoSerializer(obj, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        try:
+            obj = Endereco.objects.get(id=id)
+
+        except Endereco.DoesNotExist:
+            msg = {"msg": "Endereco não encontrado"}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+
+        obj.delete()
+        return Response({"msg":"deletado"}, status=status.HTTP_204_NO_CONTENT)
+
+class EstabelecimentoAPI(APIView):
+    def get(self, request, id=None):
+        if id is None:
+            # Se id não for fornecido, listamos todos os usuários
+            queryset = Estabelecimento.objects.all()
+            serializer = EstabelecimentoSerializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            # Se id for fornecido, obtemos um único usuário
+            try:
+                obj = Estabelecimento.objects.get(pk=id)
+                serializer = EstabelecimentoSerializer(obj)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except Estabelecimento.DoesNotExist:
+                msg = {"msg": "Estabelecimento não encontrado"}
+                return Response(status=status.HTTP_404_NOT_FOUND, data=msg)
+        serializer = EstabelecimentoSerializer(obj)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = EstabelecimentoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, id):
+        try:
+            obj = Estabelecimento.objects.get(id=id)
+
+        except Estabelecimento.DoesNotExist:
+            msg = {"msg": "Estabelecimento não encontrado"}
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = EstabelecimentoSerializer(obj, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        try:
+            obj = Estabelecimento.objects.get(id=id)
+
+        except Estabelecimento.DoesNotExist:
+            msg = {"msg": "Estabelecimento não encontrado"}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+
+        obj.delete()
+        return Response({"msg":"deletado"}, status=status.HTTP_204_NO_CONTENT)
